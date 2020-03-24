f62276fa9ca052eb9fd51b1c26605b1240754595
Catharina <catharina.sorum@outlook.com>
2020-03-24 12:15:33 +0100

    added

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,58 @@
-<page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script>
-    let message = "Blank Svelte Native App"
+    import FirestoreParser from 'firestore-parser'
+
+    let items = []
+    const baseUrl = 'https://firestore.googleapis.com/v1/'
+    const productsUrl = baseUrl + 'projects/svelte-native-firebase/databases/(default)/documents/products'
+
+    const getProducts = () => {
+        fetch(productsUrl)
+            .then( response => response.json() )
+                .then( json => FirestoreParser(json) )
+                    .then( parsed => {
+                        items = parsed.documents
+                        console.log(items)
+                     })
+            .catch( error => console.log(error) )
+    }
+
+    getProducts()
+
 </script>
 
+<page>
+    <actionBar title="Svelte Firestore REST" />
+    <scrollView class="main">
+        <stackLayout>
+            {#each items as item}
+                <flexboxLayout class="product">
+                    <stackLayout>
+                        <label class="h2" text={item.fields.title} />
+                        <label class="body" text={item.fields.price}$ />
+                    </stackLayout>
+                    <image src={item.fields.image} stretch="aspectFit" />
+                </flexboxLayout>
+            {:else}
+                <activityIndicator busy={true} />
+            {/each}
+        </stackLayout>
+    </scrollView>
+</page>
+
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .main {
+        background-color:#fff;
     }
-    .info {
-        font-size: 20;
+    .product {
+        margin: 20;
+        padding: 15;
+        background-color: #eee;
+        border-radius:8;
+        flex-direction: column;
+        text-align: center;
     }
+    .product > image {
+        height:130;
+    }
+
 </style>
